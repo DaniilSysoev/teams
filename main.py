6ab152a533f9420e42023decc501388b248aa54a
Daniil <sysoevdaniil@mail.ru>
2022-09-29 00:33:34 +0300

    second commit

--- main.py
+++ main.py
@@ -48,44 +48,89 @@ while True:
                 body={'values': [array[i]]}).execute()
             count += 1
     elif mode == "2":
+        op = ["ИБ", "КБ", "ИТСС", "ИВТ", "ПМ"]
+        miem = {}
+        not_miem = {}
+        for i in array.keys():
+            if i in op:
+                miem[i] = array[i]
+            else:
+                not_miem[i] = array[i]
         #Распределение по командам
         teams = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: []}
         i = 1
         was = []
         people_count = 0
-        length = sum([len(array[i]) for i in array])
+        length_miem = sum([len(miem[i]) for i in miem])
         number_of_teams = 15
+        miem_keys = list(miem.keys())
         while i <= number_of_teams:
             team_count = 0
-            while team_count < (length//number_of_teams + 1):
-                for j in range(len(keys)):
+            while team_count < (length_miem//number_of_teams + 1):
+                for j in range(len(miem_keys)):
                     op_count = 0
-                    for t in range(len(array[keys[j]])):
-                        if array[keys[j]][t] in was and op_count >= ((length//number_of_teams + 1)//2):
+                    for t in range(len(miem[miem_keys[j]])):
+                        if miem[miem_keys[j]][t] in was and op_count >= ((length_miem//number_of_teams + 1)//2 + 1):
                             break
-                        elif op_count < 11:
-                            if not(array[keys[j]][t] in was):
-                                teams[i].append((keys[j], array[keys[j]][t]))
+                        elif op_count < ((length_miem//number_of_teams + 1)//2 + 1):
+                            if not(miem[miem_keys[j]][t] in was):
+                                teams[i].append((miem_keys[j], miem[miem_keys[j]][t]))
                                 team_count += 1
-                                was.append(array[keys[j]][t])
+                                was.append(miem[miem_keys[j]][t])
                                 op_count += 1
                                 people_count += 1
                         else:
                             break
-                        if people_count == length:
+                        if people_count == length_miem:
                             break
-                        if team_count >= (length//number_of_teams + 1):
+                        if team_count >= (length_miem//number_of_teams + 1):
                             break
-                    if people_count == length:
+                    if people_count == length_miem:
                         break
-                    if team_count >= (length//number_of_teams + 1):
+                    if team_count >= (length_miem//number_of_teams + 1):
                         break
-                if people_count == length:
+                if people_count == length_miem:
                     break
             i += 1
-            if people_count == length:
+            if people_count == length_miem:
                 break
 
+        i = 1
+        was = []
+        people_count = 0
+        length_not_miem = sum([len(not_miem[i]) for i in not_miem])
+        number_of_teams = 15
+        not_miem_keys = list(not_miem.keys())
+        while i <= number_of_teams:
+            team_count = 0
+            while team_count < (length_not_miem // number_of_teams +1):
+                for j in range(len(not_miem_keys)):
+                    op_count = 0
+                    for t in range(len(not_miem[not_miem_keys[j]])):
+                        if not_miem[not_miem_keys[j]][t] in was and op_count >= ((length_not_miem // number_of_teams + 1) // 2 + 1):
+                            break
+                        elif op_count < ((length_not_miem // number_of_teams + 1) // 2 + 1):
+                            if not (not_miem[not_miem_keys[j]][t] in was):
+                                teams[i].append((not_miem_keys[j], not_miem[not_miem_keys[j]][t]))
+                                team_count += 1
+                                was.append(not_miem[not_miem_keys[j]][t])
+                                op_count += 1
+                                people_count += 1
+                        else:
+                            break
+                        if people_count == length_not_miem:
+                            break
+                        if team_count >= (length_not_miem // number_of_teams + 1):
+                            break
+                    if people_count == length_not_miem:
+                        break
+                    if team_count >= (length_not_miem // number_of_teams + 1):
+                        break
+                if people_count == length_not_miem:
+                    break
+            i += 1
+            if people_count == length_not_miem:
+                break
         #Здесь я чисто для себя выводил в консоль все команды и размеры команд, чтобы найти ошибки, но если вдруг понадобится что то изменить в логике работы, то можно это раскоментить
         #print(" ".join([str(len(i)) for i in teams.values()]))
         #print("\n".join([str(teams[i]) for i in range(1, 16)]))
@@ -95,7 +140,7 @@ while True:
         keys = list(array.keys())
         sheet.values().clear(
             spreadsheetId="",
-            range="Команды"
+            range="Команды(старое)"
         ).execute()
         #Запись на лист по командам в вертикальном порядке, но не больше 26 команд (нужно доработать, чтобы было больше команд)
         alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -104,12 +149,12 @@ while True:
             send = [["{0} {1}".format(j[0], j[1])] for j in teams[i]]
             resp = sheet.values().update(
                 spreadsheetId="",
-                range='Команды!' + alphabet[count] + "1",
+                range='Команды(старое)!' + alphabet[count] + "1",
                 valueInputOption='RAW',
                 body={'values': [[i]]}).execute()
             resp = sheet.values().update(
                 spreadsheetId="",
-                range='Команды!' + alphabet[count] + "2",
+                range='Команды(старое)!' + alphabet[count] + "2",
                 valueInputOption='RAW',
                 body={'values': send}).execute()
             count += 1
